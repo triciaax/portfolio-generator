a0a1438782f73434b496f3fbdccea1a982293d7e
Tricia <tricianghiem@gmail.com>
2022-06-27 20:20:57 -0500

    refactor code

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const name = profileDataArgs[0];
-const github = profileDataArgs[1];
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
 
 const generatePage = (name, github) => {
   return `
@@ -23,8 +23,8 @@ const generatePage = (name, github) => {
   `;
 };
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
   console.log('Portfolio complete! Check out index.html to see the output!');
 });
